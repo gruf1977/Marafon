6357ae130a1e6671cb94d032a4c6245da247aa76
Oleg <gpuf@bk.ru>
2019-07-17 17:55:08 +0800

    Сделал первый урок

--- Obstacle/Obstacle.java
+++ Obstacle/Obstacle.java
@@ -1,6 +1,6 @@
 package Lesson_1.Marathon.Obstacle;
 
-import Lesson_1.Marathon.Competitor;
+import Lesson_1.Marathon.Competitor.Competitor;
 
 public abstract class Obstacle {
     public abstract void doIt(Competitor competitor);
